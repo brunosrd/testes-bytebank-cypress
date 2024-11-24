7334e9cdda1c43c20152018886af7e2da015914a
Brunosrd <brunosr099@outlook.com>
2024-11-24 08:35:17 -0300

    tests: cadastro, login e verificação

--- cypress/e2e/jornadaUsuario.cy.js
+++ cypress/e2e/jornadaUsuario.cy.js
@@ -19,4 +19,26 @@ describe('Jornadas de usuário', () => {
       cy.getByData('botao-sair').click();
       cy.location('pathname').should('eq', '/');
     })
+
+    it('Deve permitir que a pessoa usuária faça o seu cadastro, realize login na aplicação, realize uma transação e faça um logout', () => {
+        cy.visit('/');
+    
+        cy.getByData('botao-cadastro').click();
+        cy.getByData('nome-input').type('Gui Lima');
+        cy.getByData('email-input').type('gui@email.com');
+        cy.getByData('senha-input').type('456789');
+        cy.getByData('botao-enviar').click();
+    
+        cy.getByData('mensagem-sucesso')
+          .should('exist')
+          .and('have.text', 'Usuário cadastrado com sucesso!');
+        cy.location('pathname').should('eq', '/');
+    
+        cy.getByData('botao-login').click();
+        cy.getByData('email-input').type('gui@email.com');
+        cy.getByData('senha-input').type('456789');
+        cy.getByData('botao-enviar').click();
+    
+        cy.location('pathname').should('eq', '/home');
+      });
   })
